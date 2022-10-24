8d0a3e8e7892174665a325862137001903895e0c
andredochute <andredochute@gmail.com>
2022-10-24 18:50:16 -0300

    fix:APP.JS e Style.css

--- js/app.js
+++ js/app.js
@@ -1,35 +1,39 @@
 window.onload = () => {
-    "use strict";
-    if ("serviceWorker" in navigator) {
+    "use strict";   
+    if("serviceWorker" in navigator){
         navigator.serviceWorker.register("./sw.js");
     }
 };
-
-var constraints = {video: {facingMode: "user"}, audio: false};
-
+ 
+ 
+// configurando as constraintes do video stream
+var constraints = { video: { facingMode: "user" }, audio: false };
+// capturando os elementos em tela
 const cameraView = document.querySelector("#camera--view"),
-cameraOutput = document.querySelector("#camera--output"),
-cameraSensor = document.querySelector("#camera--sensor"),
-cameraTrigger = document.querySelector("#camera--trigger")
-
-function cameraStart(){
-    navigator.midiaDevices
-    .getUserMedia(constraints)
-    .then(function(stream){
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger")
+ 
+//Estabelecendo o acesso a camera e inicializando a visualização
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
         track = stream.getTracks()[0];
         cameraView.srcObject = stream;
     })
-    .catch(function(error){
-        console.error("Oops. Somethins is broken.", error);
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
     });
 }
-
-cameraTrigger.onclick = function(){
+ 
+// Função para tirar foto
+cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("token");
+    cameraOutput.classList.add("taken");
 };
-
+// carrega imagem de camera quando a janela carregar
 window.addEventListener("load", cameraStart, false);
